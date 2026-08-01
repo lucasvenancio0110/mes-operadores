128859655b590a71a09f857ba2b1215229926039
lucasvenancio0110 <lucassantanals0110@gmail.com>
2026-08-01 19:52:23 -0300

    Adiciona configuração da API Cloudflare

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  // Será preenchido na próxima etapa com a URL do Cloudflare Worker.
+  // Exemplo: "https://mes-operadores-api.seu-subdominio.workers.dev"
+  cloudApiUrl: ""
+};
