c23d3d36d0b9925868f0b6c599d03f77021697da
lucasvenancio0110 <lucassantanals0110@gmail.com>
2026-08-02 14:18:06 -0300

    Renova cache PWA para versão 3.0.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'neodent-mes-v3.0.1';
+const VERSION = 'neodent-mes-v3.0.2';
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const APP_SHELL = [
