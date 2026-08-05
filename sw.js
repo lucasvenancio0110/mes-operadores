ac69d37fbf3eb43f6870d76b6d440886aa77a3d7
lucasvenancio0110 <lucassantanals0110@gmail.com>
2026-08-05 18:07:24 -0300

    chore: renovar cache do PWA para 5.0.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'neomes-v4.0.0-secure-auth-admin-v5.0.6-advisory-pointing';
+const VERSION = 'neomes-v4.0.0-secure-auth-admin-v5.0.7-turn-target-releases';
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const BASE = self.registration.scope;
